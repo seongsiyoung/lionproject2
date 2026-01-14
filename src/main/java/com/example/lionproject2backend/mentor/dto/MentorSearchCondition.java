23f7b7ff187b770137c7990f2184dc71b757b4c2
parkyejin0817 <sk47304842@icloud.com>
2026-01-14 09:53:19 +0900

    wip: querydsl 작업 중 임시 저장

--- src/main/java/com/example/lionproject2backend/mentor/dto/MentorSearchCondition.java
+++ src/main/java/com/example/lionproject2backend/mentor/dto/MentorSearchCondition.java
@@ -17,26 +17,25 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MentorSearchCondition {
+
+    // 기술 스택목록
     private List<String> skills;
 
-    //멘토 닉네임
+    // 멘토 닉네임
     private String nickname;
 
-    //최소 가격
+    // 최소 가격
     private Integer minPrice;
 
-    //최대 가격
+    // 최대 가격
     private Integer maxPrice;
-
-    //최소 평점
-    private Double minRating;
 
     /**
      * 정렬 기준
-     * - rating: 평점 높은순
-     * - reviewCount: 리뷰 많은순
+     * - reviewCount: 리뷰 많은 순
      * - priceAsc: 낮은 가격순
-     * - priceDesc: 가격 높은순
+     * - priceDesc: 높은 가격순
+     * - 기본: 최신 등록순
      */
     private String sortBy;
 }
--- src/main/java/com/example/lionproject2backend/mentor/repository/querydsl/custom/MentorRepositoryImpl.java
+++ src/main/java/com/example/lionproject2backend/mentor/repository/querydsl/custom/MentorRepositoryImpl.java
@@ -1,4 +1,11 @@
 package com.example.lionproject2backend.mentor.repository.querydsl.custom;
 
-public class MentorRepositoryImpl {
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MentorRepositoryImpl implements MentorRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
 }
